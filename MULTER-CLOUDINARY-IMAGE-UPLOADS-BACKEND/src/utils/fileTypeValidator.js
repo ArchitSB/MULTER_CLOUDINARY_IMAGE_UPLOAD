1cf3fed03aea28e197c249464203cbc4cf5ff3f9
Archit S Baghel <arsiba999@gmail.com>
2025-01-04 23:11:20 +0530

    project completed

---
+++ MULTER-CLOUDINARY-IMAGE-UPLOADS-BACKEND/src/utils/fileTypeValidator.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const fileTypeValidator = (file) => {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimeType = fileTypes.test(file.mimetype); //'image/png
+    return extname && mimeType;
+};
+
+module.exports = { fileTypeValidator };
---
+++ ms2-cloudinary-frontend-main/src/constants/imageUpload.js
@@ -0,0 +1,2 @@
+export const FILE_UPLOAD_URL = 'http://localhost:4040/files/upload';
+export const JWT_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZXNzb24iOiJjbG91ZGluYXJ5VXBsb2FkIiwiaWF0IjoxNzM2MDA1MTQzfQ.iGoEbKEwZA5kbkSdh6UjfBENlhDmlt9U75avZjrjmsY';
